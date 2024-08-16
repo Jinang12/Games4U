bb7bdf172710587451540aee64aa7870b8075c4d
Jinang12 <156568628+Jinang12@users.noreply.github.com>
2024-08-17 00:31:53 +0530

    Delete RockPaperScissor.js

--- RockPaperScissor.js
+++
@@ -1,5 +0,0 @@
-const obj={
-    0:'https://t3.ftcdn.net/jpg/00/55/21/18/360_F_55211893_pMzGwbg4p7yElUGfc868m9dUXaknoWkU.jpg" alt="Rock" style="width: 50px; height: 50px;',
-    1:'https://t4.ftcdn.net/jpg/03/03/60/99/360_F_303609961_q026RoeUYia0Ufp6nrKjrqdGcgRyjaxQ.jpg',
-    2:'https://static.vecteezy.com/system/resources/previews/004/606/141/original/simple-cartoon-icon-scissors-on-a-white-background-cartoon-vector.jpg" alt="Scissors" style="width: 50px; height: 50px; margin-top: 20px;'
-};
